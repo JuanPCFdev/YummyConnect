61a6975794800bf6fed7a71e719069b213cacb66
Juan Cuellar <144962816+JuanPCFdev@users.noreply.github.com>
2025-05-26 20:00:46 -0500

    New layout of home screen
    A new Layout to edit of the HomeScreen, With a Scaffold, top app bar and navigation bottom bar

---
+++ app/src/main/java/com/jpdev/yummyconnect/presentation/screens/HomeScreen.kt
@@ -0,0 +1,168 @@
+package com.jpdev.yummyconnect.presentation.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jpdev.yummyconnect.R
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    MaterialTheme {
+        HomeScreen(Modifier)
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = { TopBar() },
+        bottomBar = { BottomBar() }
+    ) { innerPadding ->
+        HomeContent(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun HomeContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+
+    }
+}
+
+@Composable
+fun TopBar() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            "YummyConnect",
+            modifier = Modifier.padding(16.dp),
+            color = MaterialTheme.colorScheme.primary,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        )
+        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            //Icon for notifications
+            Icon(
+                painter = painterResource(R.drawable.ic_notification),
+                contentDescription = "Notifications icon",
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(8.dp)
+            )
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            //Profile picture
+            Image(
+                painter = painterResource(R.drawable.ic_profile),
+                contentDescription = "Profile picture",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(40.dp)
+                    .border(
+                        width = 2.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(corner = CornerSize(100.dp))
+                    )
+                    .clip(shape = CircleShape)
+            )
+        }
+    }
+}
+
+@Composable
+fun BottomBar() {
+    NavigationBar {
+        NavigationBarItem(
+            selected = true,
+            onClick = {},
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_home),
+                    contentDescription = "Home icon"
+                )
+            },
+            label = { Text("Home") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_search),
+                    contentDescription = "Search icon"
+                )
+            },
+            label = { Text("Discover") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_add),
+                    contentDescription = "Create icon"
+                )
+            },
+            label = { Text("Create") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_saved),
+                    contentDescription = "Saved icon"
+                )
+            },
+            label = { Text("Saved") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {},
+            icon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_profile_navigation),
+                    contentDescription = "Profile icon"
+                )
+            },
+            label = { Text("Profile") }
+        )
+    }
+}
